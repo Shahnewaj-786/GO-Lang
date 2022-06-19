71405c5dddde26d3c48f9b208b26b9b33bef54f0
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-19 21:26:29 +0600

    Data types

--- main.go
+++ main.go
@@ -5,11 +5,29 @@ Escape Sequence: \n --> newline; \t --> tab;
 
 */
 
+/*
+
+Data types and Keywords:
+
+Datatypes: boolean, string, numeric - integer, floating, dreived types- pointer, array, slice, map, interface ect.
+
+
+Variations of intwger --> int8 (8 bits), int16, int32, int64
+
+Variations of float --> float32, float64
+
+
+*/
+
 package main
 
 import "fmt" //fmt means format
 
 func main() { // func means function
+
+	//int8 --> 2^8 = 256 which is -128 to 127
+	//int16 -- 2^16
+	//uint8 --> 2^8 = 256 which is 0 to 255
 
 	fmt.Println("First program on GO")
 	fmt.Print("Learning new things is so much fun.")
