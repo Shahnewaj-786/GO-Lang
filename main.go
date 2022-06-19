285198665fb4cc50ffc1f51bb72f55c1cf049faf
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-19 21:14:58 +0600

    Comments and Escape Sequesces

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
+/* Package is the main thing for GO lang
+
+
+Escape Sequence: \n --> newline; \t --> tab;
+
+*/
+
 package main
 
-import "fmt"
+import "fmt" //fmt means format
 
-func main() {
+func main() { // func means function
 
 	fmt.Println("First program on GO")
 	fmt.Print("Learning new things is so much fun.")
