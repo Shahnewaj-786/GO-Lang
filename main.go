86f79474cd8eb47b1f0c6af5aeeee4f2c28dfd27
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-19 20:52:09 +0600

    Course plan, Environment setup and First program

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("First program on GO")
+	fmt.Print("Learning new things is so much fun.")
+	fmt.Print("Go is great for multithread programming.")
+
+}
