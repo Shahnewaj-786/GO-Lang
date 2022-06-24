896f92b43ca4632977e69e7a337c831eadefec11
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-25 00:59:56 +0600

    Taking user input

---
+++ User_Input/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var num1, num2 int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+
+	fmt.Print("Enter two numbers: ")
+	fmt.Scan(&num1, &num2)
+
+	fmt.Printf("My name is %v \n", name)
+	fmt.Printf("Number1 is %v and Number2 is %v \n", num1, num2)
+}
