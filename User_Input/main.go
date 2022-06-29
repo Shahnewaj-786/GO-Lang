6d4670e65a34399cf3eb708d72e478f931b99714
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-29 21:45:26 +0600

    taking user input

--- User_Input/main.go
+++ User_Input/main.go
@@ -12,6 +12,9 @@ func main() {
 	fmt.Print("Enter two numbers: ")
 	fmt.Scan(&num1, &num2)
 
+	/*fmt.Print("Enter two numbers: ")
+	fmt.Scanf("%v %v", &num1, &num2)*/ //problem with takeing variable value using Scanf
+
 	fmt.Printf("My name is %v \n", name)
 	fmt.Printf("Number1 is %v and Number2 is %v \n", num1, num2)
 
