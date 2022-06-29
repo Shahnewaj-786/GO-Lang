14a2b58b6fbffb2139ef8cfbf7db99989791390a
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-29 22:08:26 +0600

    number formatting

--- main.go
+++ main.go
@@ -31,9 +31,9 @@ func main() { // func means function
 	//int16 -- 2^16
 	//uint8 --> 2^8 = 256 which is 0 to 255
 
-	fmt.Println("First program on GO")
+	/*fmt.Println("First program on GO")
 	fmt.Print("Learning new things is so much fun.")
-	fmt.Print("Go is great for multithread programming. \n")
+	fmt.Print("Go is great for multithread programming. \n")*/
 
 	//Variables
 
@@ -45,64 +45,74 @@ func main() { // func means function
 
 
 	*/
-	//Variable Declaration
+	/*
+		//Variable Declaration
 
-	var name, uni string
-	var age int
-	var CG float32
+		var name, uni string
+		var age int
+		var CG float32
 
-	// Variable initialization
+		// Variable initialization
 
-	name = "Bappi"
-	uni = "AIUB"
-	age = 22
-	CG = 3.3
+		name = "Bappi"
+		uni = "AIUB"
+		age = 22
+		CG = 3.3
 
-	fmt.Println("My name is", name)
-	fmt.Println("My age is", age)
-	fmt.Println("My University is", uni)
-	fmt.Println("My CGPA is", CG)
+		fmt.Println("My name is", name)
+		fmt.Println("My age is", age)
+		fmt.Println("My University is", uni)
+		fmt.Println("My CGPA is", CG)
 
-	fmt.Println(" \n ")
+		fmt.Println(" \n ")
 
-	// Variable Declaration and Variable initialization togather
+		// Variable Declaration and Variable initialization togather
 
-	var name_1 = "Shahnewaj"
-	var uni_1 = "AIUB"
-	var age_1 = 24
-	var CG_1 = 2.9
+		var name_1 = "Shahnewaj"
+		var uni_1 = "AIUB"
+		var age_1 = 24
+		var CG_1 = 2.9
 
-	fmt.Println("My name is", name_1, "I am ", age_1, "years old. \n", "My University is", uni_1, "and my CGPA is", CG_1)
+		fmt.Println("My name is", name_1, "I am ", age_1, "years old. \n", "My University is", uni_1, "and my CGPA is", CG_1)
 
-	newvariable := "We can declear variable without var key word. We just have to use _:_ before _=_ like \" CG := 2.0 \" \n"
+		newvariable := "We can declear variable without var key word. We just have to use _:_ before _=_ like \" CG := 2.0 \" \n"
 
-	fmt.Print(newvariable)
+		fmt.Print(newvariable)
 
-	// Use of %v and Printf and Constant
+		// Use of %v and Printf and Constant
 
-	address := "Kalabagan"
-	mobile := "0123333333"
-	const HOME = "BD"
+		address := "Kalabagan"
+		mobile := "0123333333"
+		const HOME = "BD"
 
-	fmt.Printf("My address is %v and my number is %v and my country is %v.", address, mobile, HOME)
+		fmt.Printf("My address is %v and my number is %v and my country is %v.", address, mobile, HOME)
 
-	//Getting User input
+		//Getting User input
 
-	var fullname_ui string
-	var num1_ui, num2_ui int
+		var fullname_ui string
+		var num1_ui, num2_ui int
 
-	fmt.Print("Enter your name: ")
-	fmt.Scan(&fullname_ui)
+		fmt.Print("Enter your name: ")
+		fmt.Scan(&fullname_ui)
 
-	fmt.Print("Enter two numbers: ")
-	fmt.Scan(&num1_ui, &num2_ui)
+		fmt.Print("Enter two numbers: ")
+		fmt.Scan(&num1_ui, &num2_ui)
 
-	fmt.Printf("My name is %v \n", fullname_ui)
-	fmt.Printf("num1 is %v and num2 is %v \n", num1_ui, num2_ui)
+		fmt.Printf("My name is %v \n", fullname_ui)
+		fmt.Printf("num1 is %v and num2 is %v \n", num1_ui, num2_ui)*/
 
 	/*var float float32
 	fmt.Print("Enter float number: ")
 	fmt.Scanf("%v", &float)
-	fmt.Printf("Float numver is %v", float)*/
+	fmt.Printf("Float numver is %v", float)
+	*/
 
+	var decimalNumber int
+
+	fmt.Printf("Decimal Number= ")
+	fmt.Scanf("%v", &decimalNumber)
+
+	fmt.Printf("Binary Number = %b\n", decimalNumber)
+	fmt.Printf("Octal Number = %o\n", decimalNumber)
+	fmt.Printf("Hexadecimal Number = %x\n", decimalNumber)
 }
