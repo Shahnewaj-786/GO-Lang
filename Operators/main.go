9e0ee8920fda7aae2f49a07ef44b084f2389f959
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-30 14:23:25 +0600

    Bitwise operator

--- Operators/main.go
+++ Operators/main.go
@@ -4,7 +4,7 @@
 3.Unary --> ++ --
 4.Relational --> < > == != !< !>
 5.Logical --> && || !
-6.Bitwise
+6.Bitwise --> & | ^ >> <<
 7.Spical
 */
 
@@ -64,5 +64,13 @@ func main() {
 	l_z := l_x > 5 && l_x > 9
 
 	fmt.Printf("l_z = %v\n", l_z)
+
+	//Bitwise
+
+	b_x := 18 // 18 = 10010 (binary)
+	b_y := 17 // 17 = 10001 (binary)
+	b_z := b_x & b_y
+
+	fmt.Printf("Bitwise AND= %v\n", b_z)
 
 }
