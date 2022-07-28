6fb91151c57470247f1009414453ee14f8f14e3d
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-07-29 01:36:22 +0600

    sue of for loop

--- Loop_Control_Statement/main.go
+++ Loop_Control_Statement/main.go
@@ -18,4 +18,28 @@ func main() { // GO lang has only one Loop Control Statement which is for loop
 		fmt.Printf("%v\n", z)
 		z--
 	}
+
+	//Print Even Numbers
+	for even := 2; even <= 100; even = even + 2 {
+		fmt.Printf("%v\n", even)
+	}
+	fmt.Printf("\n")
+	//Print Odd Numbers
+	for odd := 1; odd <= 100; odd = odd + 2 {
+		fmt.Printf("%v\n", odd)
+	}
+
+	//Print Even Numbers
+	for x := 1; x <= 100; x++ {
+		if x%2 == 0 {
+			fmt.Printf("%v\n", x)
+		}
+	}
+	fmt.Printf("\n")
+	//Print Odd Numbers
+	for y := 1; y <= 100; y++ {
+		if y%2 != 0 {
+			fmt.Printf("%v\n", y)
+		}
+	}
 }
